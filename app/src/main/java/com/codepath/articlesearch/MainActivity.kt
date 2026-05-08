224b5818a219752685263faad9e5c9c9c56e6b4e
DantheOPMan <44145044+DantheOPMan@users.noreply.github.com>
2026-05-08 01:52:29 -0400

    Update MainActivity.kt

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -10,10 +10,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
-<<<<<<< Updated upstream
-import kotlinx.coroutines.Dispatchers.IO
-=======
->>>>>>> Stashed changes
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
@@ -26,30 +22,18 @@ fun createJson() = Json {
 }
 
 private const val TAG = "MainActivity/"
-<<<<<<< Updated upstream
-private const val SEARCH_API_KEY = "c30b6be13072568f3198912087cdda39"
-private const val ARTICLE_SEARCH_URL =
-    "https://api.themoviedb.org/3/tv/top_rated?api_key=${SEARCH_API_KEY}"
-
-
-class MainActivity : AppCompatActivity() {
-    private val articles = mutableListOf<DisplayArticle>()
-=======
 private  val SEARCH_API_KEY = BuildConfig.API_KEY
 private val ARTICLE_SEARCH_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
 
 class MainActivity : AppCompatActivity() {
     private val articles = mutableListOf<DisplayArticle >()
->>>>>>> Stashed changes
     private lateinit var articlesRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         val view = binding.root
         setContentView(view)
 
@@ -61,22 +45,6 @@ class MainActivity : AppCompatActivity() {
         articlesRecyclerView.layoutManager = LinearLayoutManager(this).also {
             val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
             articlesRecyclerView.addItemDecoration(dividerItemDecoration)
-        }
-        lifecycleScope.launch {
-            (application as ArticleApplication).db.articleDao().getAll().collect { databaseList ->
-                databaseList.map { entity ->
-                    DisplayArticle(
-                        entity.headline,
-                        entity.articleAbstract,
-                        entity.byline,
-                        entity.mediaImageUrl
-                    )
-                }.also { mappedList ->
-                    articles.clear()
-                    articles.addAll(mappedList)
-                    articleAdapter.notifyDataSetChanged()
-                }
-            }
         }
 
         val client = AsyncHttpClient()
@@ -93,26 +61,6 @@ class MainActivity : AppCompatActivity() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
                 Log.i(TAG, "Successfully fetched articles: $json")
                 try {
-<<<<<<< Updated upstream
-
-                    val parsedJson = createJson().decodeFromString(
-                        SearchTvResponse.serializer(),
-                        json.jsonObject.toString()
-                    )
-                    parsedJson.response?.docs?.let { list ->
-                        lifecycleScope.launch(IO) {
-                            (application as ArticleApplication).db.articleDao().deleteAll()
-                            (application as ArticleApplication).db.articleDao().insertAll(list.map {
-                                ArticleEntity(
-                                    headline = it.headline?.main,
-                                    articleAbstract = it.abstract,
-                                    byline = it.byline?.original,
-                                    mediaImageUrl = it.mediaImageUrl
-                                )
-                            })
-                        }
-                    }
-=======
                     val parsedJson = createJson().decodeFromString(
                         SearchNewsResponse.serializer(),
                         json.jsonObject.toString()
@@ -134,13 +82,11 @@ class MainActivity : AppCompatActivity() {
                                 }
                             }
                         }
+
                     }
 
-
->>>>>>> Stashed changes
-
-                    // Reload the screen
                     articleAdapter.notifyDataSetChanged()
+
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
                 }
